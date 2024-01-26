8ac28ce8880f296e97ab6dd2ce343c055e8a879f
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-26 12:39:46 -0300

    Serializers

---
+++ product/factories.py
@@ -0,0 +1,30 @@
+import factory
+from product.models import Product
+from product.models import Category
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    title = factory.Faker("pystr")
+    slug = factory.Faker("pystr")
+    description = factory.Faker("pystr")
+    active = factory.Iterator([True, False])
+
+    class Meta:
+        model = Category
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    price = factory.Faker("pyint")
+    title = factory.Faker("pystr")
+    category = factory.LazyAttribute(CategoryFactory)
+
+    @factory.post_generation
+    def category(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for category in extracted:
+                self.cacleartegory.add(category)
+
+    class Meta:
+        model = Product
